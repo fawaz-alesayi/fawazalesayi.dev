f31c0f3c52ea639649e327b7a968e0e35af5cc63
fulayfel <fawaztsa@gmail.com>
2021-08-04 13:35:15 +0300

    configs

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: "ts-jest",
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|svelte)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.svelte$': ['svelte-jester', { preprocess: true }],
+  },
+  moduleNameMapper: {
+    '@/src/(.*)': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,35 @@
-import preprocess from 'svelte-preprocess';
-import node from '@sveltejs/adapter-node';
+import staticAdapter from '@sveltejs/adapter-static';
+import { resolve } from "path";
+import md from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import sveltePreprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [sveltePreprocess({
+    aliases: {
+      '$src': resolve('./src'),
+      '$static': resolve('./static'),
+    },
+    replace: [['API_ENDPOINT', "amazing"]],
+  }), md.mdsvex(mdsvexConfig)],
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    vite: {
+      resolve: {
+        alias: {
+          $src: resolve('./src'),
+          $static: resolve('./static'),
+        },
+      },
+    },
 
-	adapter: node(),
+    adapter: staticAdapter(),
   },
 };
 
